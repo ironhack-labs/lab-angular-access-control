724de84b8ce8102cc5fa56caf414426f84d8bd98
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-01 18:29:46 +0100

    it2 components created

---
+++ starter-code/src/app/my-home-component/my-home-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-home-component',
+  templateUrl: './my-home-component.component.html',
+  styleUrls: ['./my-home-component.component.css']
+})
+export class MyHomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ starter-code/src/app/my-movie-component/my-movie-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-movie-component',
+  templateUrl: './my-movie-component.component.html',
+  styleUrls: ['./my-movie-component.component.css']
+})
+export class MyMovieComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
