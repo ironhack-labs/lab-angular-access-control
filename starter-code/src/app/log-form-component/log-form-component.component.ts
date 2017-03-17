9c6130f01094407a46def6524262796119639277
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-03-17 16:50:51 -0400

    done

--- starter-code/src/app/log-form-component/log-form-component.component.ts
+++ starter-code/src/app/log-form-component/log-form-component.component.ts
@@ -4,20 +4,24 @@ import { AccessLogService } from '../access-log.service';
 @Component({
   selector: 'app-log-form-component',
   templateUrl: './log-form-component.component.html',
-  styleUrls: ['./log-form-component.component.css']
+  styleUrls: ['./log-form-component.component.css'],
+  providers: [AccessLogService]
 })
 export class LogFormComponentComponent implements OnInit {
 
   constructor(private myService: AccessLogService) { }
+    newLogMessage = {};
 
-  logMessages: Array<Object> = [];
-  newLogMessage: Object = {};
   ngOnInit() {
+
   }
 
-  addAccessItem() {
-    this.logMessages.push(this.newLogMessage);
-    this.newLogMessage = {};
+  addAccessItems (user) {
+    const newLogMessage = {
+      person: user.person,
+      message: user.message
+    }
+    this.myService.addAccessItem(newLogMessage.person, newLogMessage.message);
+    console.log(this.myService);
   }
-
 }
