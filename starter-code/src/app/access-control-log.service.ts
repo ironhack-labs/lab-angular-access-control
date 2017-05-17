564a6a4febd0cb9c6231d8f94c6091d837e26ae4
Bjim <imbjim1@gmail.com>
2017-05-17 16:20:33 +0200

    createdAt added

--- starter-code/src/app/access-control-log.service.ts
+++ starter-code/src/app/access-control-log.service.ts
@@ -6,9 +6,12 @@ export class AccessControlLogService {
   logMessages: Array<any> = [];
 
   addAccessItem(person, message){
-    this.logMessages.push({person, message});
+    this.logMessages.push({person, message, createdAt: new Date()});
     this.logMessages.forEach(message =>{
-      console.log(`person:${message.person} message:${message.message}`)
+      var dd = message.createdAt.getDate();
+      var mm = message.createdAt.getMonth()+1; //January is 0!
+      var yyyy = message.createdAt.getFullYear();
+      console.log(`person:${message.person} message:${message.message} createdAt:${message.createdAt}`)
     })
   }
 
