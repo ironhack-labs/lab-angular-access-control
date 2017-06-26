27d972c616904dd5e6e8c4d51aa378566251b223
Adel Acebal <adelacebal@yahoo.com>
2017-06-25 23:23:13 -0400

    services completed

--- starter-code/src/app/access-log.service.ts
+++ starter-code/src/app/access-log.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AccessLogService {
 
+  newAccessLog: Object;
+
   accessLog: Object[] = [
     { person: "Adel", message: "Training", createdAt: Date },
     { person: "Robert", message: "Emotional support", createdAt: Date},
@@ -10,5 +12,16 @@ export class AccessLogService {
   ]
 
   constructor() { }
+
+  addAccessItem(person, message): any {
+    this.newAccessLog = {
+      person, message, Date
+    }
+    this.accessLog.push(this.newAccessLog);
+  }
+
+  getAccessLog(): Object[] {
+    return this.accessLog
+  }
 
 }
