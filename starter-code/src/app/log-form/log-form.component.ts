458b751adbee179b3c7754b708db03f3c9d9842e
Daniele <daniele.germani.85@gmail.com>
2017-07-26 23:36:29 +0200

    iteration 3

--- starter-code/src/app/log-form/log-form.component.ts
+++ starter-code/src/app/log-form/log-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccessControlLogService } from '../service/access-control-log.service'
+
+
 @Component({
   selector: 'app-log-form',
   templateUrl: './log-form.component.html',
@@ -7,13 +9,16 @@ import { AccessControlLogService } from '../service/access-control-log.service'
   providers: [AccessControlLogService]
 })
 export class LogFormComponent implements OnInit {
+  logMessages:Array<Object> = []
 
   constructor(public logea:AccessControlLogService) { }
 
   ngOnInit() {
   }
   submitForm(myForm){
-    console.log(myForm)
+    console.log(myForm.form.value)
     this.logea.addAccessItem(myForm.form._value.person, myForm.form._value.message)
+    this.logMessages = this.logea.getAccessLog();
+    console.log(this.logMessages)
   }
 }
