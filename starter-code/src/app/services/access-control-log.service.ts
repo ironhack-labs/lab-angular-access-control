fafca343c6605128505dcc631db6891d996483f9
Denis Barriere <denis.barriere@gmail.com>
2017-07-26 17:24:31 +0200

    done

---
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccessControlLogService {
+
+  logMessages: Array<{ person: string, message: string, createdAt: Date}>;
+
+  constructor() { this.logMessages = []; }
+
+  addAccessItem(person: string, message: string) {
+    let createdAt = new Date();
+    this.logMessages.push({ person, message, createdAt });
+  }
+
+  getAccessLog() {
+    return this.logMessages;
+  }
+
+}
