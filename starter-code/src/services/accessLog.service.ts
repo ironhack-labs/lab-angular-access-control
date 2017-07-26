93e00fa9ba9db44561977966d936669543dbd564
Mario Martin <mariomarsan7@gmail.com>
2017-07-26 20:14:50 +0200

    Show list of access log in Browser's console

--- starter-code/src/services/accessLog.service.ts
+++ starter-code/src/services/accessLog.service.ts
@@ -19,13 +19,13 @@ export class AccessControlLog {
       this.createdAt = new Date();
     }
     getAccessLog(){
-      return this.logs;
+      return console.log(this.logs);
     }
     addAccessItem(person: string, message:string){
       this.createdAt = new Date();
       let newObj = {person: person, message: message, createdAt: this.createdAt};
       this.logs.unshift(newObj);
-      console.log('LOGS',this.logs);
+      this.getAccessLog();
     }
 
 }
