5df68e9cd7001e26642b421ac334d1c350c2777a
Mi6u3l <miguelbragagomes@gmail.com>
2017-07-26 15:29:22 +0200

    ran beautify

--- starter-code/src/app/services/access-control-log.service.ts
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable
+} from '@angular/core';
 
 @Injectable()
 export class AccessControlLogService {
-  logMessages: Array<{person: string, message: string, createdAt: Date}> = [];
-  constructor() { }
+  logMessages: Array < {
+    person: string,
+    message: string,
+    createdAt: Date
+  } > = [];
+  constructor() {}
 
   addAccessItem(person, message) {
     this.logMessages.push({
       person,
       message,
       createdAt: new Date()
-    });  
+    });
   }
 
   getAccessLog() {
