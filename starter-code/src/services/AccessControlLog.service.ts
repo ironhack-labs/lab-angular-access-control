9023eb20913604e0a8baac158bba0cb0c4305ae2
Marc Palatsí <mpalatsi@me.com>
2017-07-26 18:51:31 +0200

    first iteration

---
+++ starter-code/src/services/AccessControlLog.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccessControlLog {
+    // User: Object {
+    //   person: string,
+    //   messaje: string,
+    //   createdAt: new Date()
+    // }
+    logMessages: Array<any> = [{
+      person: String,
+      messaje: String,
+      createdAt: new Date()
+    }]
+
+    constructor() { }
+
+getAccessLog() {
+  return this.logMessages
+}
+
+addAccessItem(person: string, messaje: string) {
+  let member: Object = {
+    person : person,
+    messaje : messaje
+  }
+  this.logMessages.push(member)
+}
+
+}
