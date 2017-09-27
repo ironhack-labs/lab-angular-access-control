77fd1f2126c9bbba203eefcc60b86728d93ef332
luisa86 <lfsh86@gmail.com>
2017-09-27 16:01:57 +0200

    iteracion 1 Service Creation DONE

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
+import {AccesLogService } from './services/acces-log.service'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccesLogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ starter-code/src/app/services/acces-log.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+
+
+interface AccessObj{
+  person: string
+  message: string
+  createdAt: Date
+
+}
+
+@Injectable()
+export class AccesLogService {
+
+
+access: Array<AccessObj>
+
+
+
+  constructor() { }
+
+  getAccessLog(){
+    return this.access
+  }
+
+  addAccessItem(person:string, message:string){
+
+    this.access.push({person:person, message:message, createdAt: new Date()} )
+  }
+}
