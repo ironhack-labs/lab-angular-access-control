730ed74d7bc5342df386236899d650d62df88b0b
wba0 <william.bonneyache@gmail.com>
2017-09-27 17:13:58 -0400

    working on iteration 2

--- starter-code/src/app/logformcomponent/logformcomponent.component.ts
+++ starter-code/src/app/logformcomponent/logformcomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccesscontrollogService } from '../services/accesscontrollog.service';
 
 @Component({
   selector: 'app-logformcomponent',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogformcomponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public accessThing: AccesscontrollogService
+  ) { }
 
   ngOnInit() {
   }
--- starter-code/src/app/services/accesscontrollog.service.ts
+++ starter-code/src/app/services/accesscontrollog.service.ts
@@ -3,10 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AccesscontrollogService {
   logMessages: any = [];
+  thePerson: any = {};
+
   constructor() { }
 
-  addAcessItem(person, message){
-
+  addAcessItem(person: string, message: string){
+    this.thePerson = {
+      person: person,
+      message: message,
+      createdAt: new Date()
+    };
+    this.logMessages.push(this.thePerson);
   }
   getAccessLog(){
     return this.logMessages;
