0b6dd9b02d5d6a8e15d4c1fbad3b6e20501ffa0e
acampos91 <acampos91@hotmail.com>
2017-11-29 16:04:45 +0100

    first iteration

---
+++ starter-code/src/app/services.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServicesService {
+  access : Array<Object> = [{
+    person : String,
+    message : String,
+    createdAt : Date
+  }]
+
+  constructor() { }
+getAccessLog(){
+  return this.access
+}
+addAccessItem(person, message){
+  this.access.push(person, message)
+}
+}
