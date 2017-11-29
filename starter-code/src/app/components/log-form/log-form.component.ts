d4b14af2f03e6b986284f5d060134658d1c8f10a
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-29 22:58:39 +0100

    done

---
+++ starter-code/src/app/components/log-form/log-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { AccessControlLogService } from '../../services/access-control-log.service';
+import { setTimeout } from 'timers';
+
+
+@Component({
+  selector: 'app-log-form',
+  templateUrl: './log-form.component.html',
+  styleUrls: ['./log-form.component.css']
+})
+export class LogFormComponent implements OnInit {
+
+  newLog: object = this.theAccessControl;
+  person: string;
+  message: string;
+
+  submitted = false;
+  feedbackEnabled = false;
+
+  constructor(public theAccessControl: AccessControlLogService) {  }
+
+  ngOnInit() {
+  }
+
+  submitForm(myForm) {
+    if (myForm.valid) {
+      this.theAccessControl.addAccessItem(this.person, this.message);
+      this.person = null;
+      this.message = null;
+      this.submitted = true;
+      this.feedbackEnabled = false;
+      setTimeout(() => {
+        this.submitted = false;
+      }, 2000);
+      this.theAccessControl.getAccessLog();
+    } else {
+      this.feedbackEnabled = true;
+      setTimeout(() => {
+        this.feedbackEnabled = false;
+        this.person = null;
+      }, 2000);
+      // this.message = null;
+    }
+  }
+
+}
---
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccessControlLogService {
+
+  logMessages = [];
+
+
+  constructor() { }
+
+  addAccessItem(person, message) {
+    const newLog: object = {
+      name: person,
+      message: message,
+      createdAt: new Date
+    };
+    this.logMessages.push(newLog);
+   }
+
+  getAccessLog(){
+   this.logMessages.forEach((el, index) => (
+     console.log(`${el.name} logged in to ${el.message} at ${el.createdAt}`)
+   ));
+  }
+
+}
