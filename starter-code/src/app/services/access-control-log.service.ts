4a4f334c3c53da91da77a3e24884578e5516f7de
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-21 17:18:17 +0100

    done

--- starter-code/src/app/services/access-control-log.service.ts
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AccessControlLogService {
   logMessages: Array<{person: string, message: string, createdAt: Date}>;
+  
 
   constructor() { }
 
   addAccessItem(person, message){
-
+    
+    console.log('We are adding sthg');
   }
 
   getAccesLog(){
