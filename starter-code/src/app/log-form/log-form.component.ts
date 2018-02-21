28805d30ea3b305ec702ed48f0ff3452fc084f1e
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-02-21 17:33:35 +0100

    terminado

---
+++ starter-code/src/app/log-form/log-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AccessControlLog } from '../services/accesscontrollog.service';
+
+@Component({
+  selector: 'app-log-form',
+  templateUrl: './log-form.component.html',
+  styleUrls: ['./log-form.component.css'],
+  providers: [AccessControlLog]
+})
+export class LogFormComponent implements OnInit {
+  registered: boolean = false;
+
+  constructor(public theAccess: AccessControlLog) { }
+
+  ngOnInit() {
+  }
+  addAccessItem(p, m, x){
+    if (x.invalid == false) {
+    this.theAccess.addAccessItem(p,m);
+    this.registered = true;
+    let arr = this.theAccess.getAccessLog();
+    arr.shift();
+    arr.forEach( e => {
+      console.log(e);
+    })
+    }
+  }
+}
