d66259defd4b126ff9f02b49c8e26459bb847451
A-Tr <alvarotrancon@gmail.com>
2018-05-02 17:11:41 +0200

    test

--- starter-code/src/app/access-log.service.ts
+++ starter-code/src/app/access-log.service.ts
@@ -3,12 +3,20 @@ import { User } from '../app/user-interface';
 
 @Injectable()
 export class AccessLogService {
-  logMessages: Array<User>;
+  logMessages: Array<User>=[];
+
+  constructor() {}
+
+  addAccessItem = (person, message) => {
+    let newRegister = {
+      person,
+      message,
+      createdAt: new Date()
+    };
+    this.logMessages.push(newRegister);
+  }
 
   getAccessLog = () => this.logMessages;
 
-  addAccessItem(person, message) {
-    this.logMessages.push(person, message);
-  };
-  constructor() {}
+
 }
