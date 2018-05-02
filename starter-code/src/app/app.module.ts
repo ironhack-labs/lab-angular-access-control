a11a1c20428625960d3eb0c16c40621570720ae9
cosmihai <cos_mihai23@yahoo.com>
2018-05-02 20:30:20 +0200

    BCn Mihai

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LogFormComponentComponent } from './components/log-form-component/log-form-component.component';
+import { AccessLogService } from './services/access-log.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccessLogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
