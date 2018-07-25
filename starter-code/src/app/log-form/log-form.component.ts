8d11a27478601ba238c68d7f9a2b69e6157ca20a
Verónica Reina <veroreinah@gmail.com>
2018-07-25 15:43:35 +0200

    Iteration 2

---
+++ starter-code/src/app/log-form/log-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { AccessControlLogService } from "services/access.service";
+
+@Component({
+  selector: "app-log-form",
+  templateUrl: "./log-form.component.html",
+  styleUrls: ["./log-form.component.css"],
+  providers: [AccessControlLogService]
+})
+export class LogFormComponent implements OnInit {
+  successMessage: string = "";
+  constructor(private accessControl: AccessControlLogService) {}
+
+  ngOnInit() {}
+
+  addAccessItem(form) {
+    if (!form.controls.person.errors && !form.controls.message.errors) {
+      this.accessControl.addAccessItem(form.value.person, form.value.message);
+      this.successMessage = "Item added.";
+    }
+  }
+}
