fcf8909b996e350966f8aeab0040e55185611919
Ricardo Campos <ricamposm@gmail.com>
2018-07-25 20:03:10 -0500

    DE MEX Ricardo Campos

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AccessControlLogService } from './access-control-log.service';
+import { LogFormComponentComponent } from './log-form-component/log-form-component.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccessControlLogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
